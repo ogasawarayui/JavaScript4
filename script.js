e4c23c4326788e1078989d369a042f96c6f5b51a
ogasawarayui <ogasawara.yui@outlook.com>
2023-12-16 22:10:52 +0900

    js4

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ if (boolean) {
 };
 /*Q2 変数に100を代入し、ifの条件がtrueの時にconsole.log();で変数の中身を表示させる処理を作ってください。*/
 var num = 100;
-if (num === true) {
+if (num === 100) {
   console.log(num);
 }
 /*Q3 変数に"JavaScript"を代入し、ifの条件がtrueの時にconsole.log();で変数の中身を表示させる処理を作ってください。*/
-var text = "JavaScript"
-if (text === true) {
+var text = "JavaScript";
+if (text === "JavaScript") {
   console.log(text);
 }
